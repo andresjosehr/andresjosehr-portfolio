c67f0674a49d07ca30993bb62f397c014ed093b0
Jose Andres <joseandreshernandezross@gmail.com>
2020-07-23 00:54:05 -0400

    Se corrigieron estilos, se programaro el comportamiento del menu principal

--- src/app/components/general/header/header.component.ts
+++ src/app/components/general/header/header.component.ts
@@ -1,17 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  
-  responsiveMenuVisible: Boolean = false;
 
-  constructor() { }
+
+
+export class HeaderComponent implements OnInit {
+
+  responsiveMenuVisible: Boolean = false;
+  pageYPosition: number;
+  
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  scroll(el) {
+    if(document.getElementById(el)) {
+      document.getElementById(el).scrollIntoView({behavior: 'smooth'});
+    } else{
+      this.router.navigate(['/home']).then(()=> document.getElementById(el).scrollIntoView({behavior: 'smooth'}) );
+    }
+    this.responsiveMenuVisible=false;
+  }
+
+  @HostListener('window:scroll', ['doSomething($event)']) 
+    doSomething(event) {
+        this.pageYPosition=window.pageYOffset     
+    }
 }
