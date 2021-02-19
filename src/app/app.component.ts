580a7c835e1168ac04781b1cf5a6c9b55fdf280e
Jose Andres Hernandez <joseandreshernandezross@gmail.com>
2021-02-19 11:29:07 -0400

    Metadata added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as AOS from 'aos';
-import { Title } from '@angular/platform-browser';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -12,12 +12,18 @@ export class AppComponent implements OnInit{
   
   constructor(
     private titleService: Title,
+    private metaService: Meta
     ){
     
   }
   ngOnInit(): void{
 
     this.titleService.setTitle( "José Andrés | Frontend Developer" );
+
+    this.metaService.addTags([
+      {name: 'keywords', content: 'Frontend, software, developer'},
+      {name: 'description', content: 'Con 4 años de experiencia desarrollando sistemas, interfaces, bots y soluciones tecnológicas  para hacer de la web un lugar mejor. En mi trabajo me gusta liderar, proponer y ejecutar ideas, escribir y refactorizar código limpio, reutilizable y escalable.'},
+    ]);
     
     
     AOS.init(); 
