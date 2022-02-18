c3b8bc206c6b9ee50c513a914b1e64f542e8f5d0
Jose Andres <joseandreshernandezross@gmail.com>
2022-02-18 10:10:47 -0400

    Bug corregido en archivo de produccion

--- production.sh
+++ production.sh
@@ -3,4 +3,4 @@ ng build --configuration production
 git checkout production
 find . -maxdepth 1 -type f -not \( -name ".git" -or -name ".gitignore" -or -name "production.sh" \) -delete
 rm -rf assets
-cp dist/andresjosehr-portfolio/* ./*
+cp -r dist/andresjosehr-portfolio/* ./*
