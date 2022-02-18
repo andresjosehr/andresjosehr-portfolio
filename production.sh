847b3e6823fbb664f741852be7aca0985553a50b
Jose Andres <joseandreshernandezross@gmail.com>
2022-02-18 10:08:51 -0400

    Archivos de produccion actualizado

--- production.sh
+++ production.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-find . -maxdepth 1 -type f -not \( -name ".git" -or -name ".gitignore" -or -name "production.sh" -or -name ".htaccess" \) -delete
+ng build --configuration production
+git checkout production
+find . -maxdepth 1 -type f -not \( -name ".git" -or -name ".gitignore" -or -name "production.sh" \) -delete
+rm -rf assets
+cp dist/andresjosehr-portfolio/* ./*
