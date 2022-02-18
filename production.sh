001b0bc4be3ebed1edf0787d93156ca03e2144fc
Jose Andres <joseandreshernandezross@gmail.com>
2022-02-18 10:25:55 -0400

    Linea anadida en archivo de produccion

--- production.sh
+++ production.sh
@@ -4,3 +4,4 @@ git checkout production
 find . -maxdepth 1 -type f -not \( -name ".git" -or -name ".gitignore" -or -name "production.sh" \) -delete
 rm -rf assets
 cp -r dist/andresjosehr-portfolio/* ./
+cp dist/andresjosehr-portfolio/.htaccess ./.htaccess
