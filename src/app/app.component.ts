b8207b3f7bb223c57384cd88a2fc35acaf91e092
thunv <thuitptit@gmail.com>
2024-02-17 22:06:20 +0700

    update my portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as AOS from 'aos';
 import { Title, Meta } from '@angular/platform-browser';
 import { TranslateService } from '@ngx-translate/core';
-import {LanguageService} from "src/app/services/language/language.service"
+import { LanguageService } from "src/app/services/language/language.service"
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 
@@ -11,31 +11,31 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  title = 'andresjosehr-portfolio';
-  
+export class AppComponent implements OnInit {
+  title = 'Nguyễn Văn Thử';
+
   constructor(
     private titleService: Title,
     private metaService: Meta,
     private translateService: TranslateService,
     private location: Location,
     private languageService: LanguageService
-    ){
-    }
-  ngOnInit(): void{
-    
+  ) {
+  }
+  ngOnInit(): void {
+
     this.languageService.initLanguage()
 
 
-    this.titleService.setTitle( "José Andrés | Frontend Developer" );
+    this.titleService.setTitle("Nguyễn Thử | Full-stack Developer");
 
     this.metaService.addTags([
-      {name: 'keywords', content: 'Frontend, software, developer'},
-      {name: 'description', content: 'Con 4 años de experiencia desarrollando sistemas, interfaces, bots y soluciones tecnológicas  para hacer de la web un lugar mejor. En mi trabajo me gusta liderar, proponer y ejecutar ideas, escribir y refactorizar código limpio, reutilizable y escalable.'},
+      { name: 'keywords', content: 'Full-stack, software, developer' },
+      { name: 'description', content: 'Con 4 años de experiencia desarrollando sistemas, interfaces, bots y soluciones tecnológicas  para hacer de la web un lugar mejor. En mi trabajo me gusta liderar, proponer y ejecutar ideas, escribir y refactorizar código limpio, reutilizable y escalable.' },
     ]);
-    
-    
-    AOS.init(); 
+
+
+    AOS.init();
 
   }
 }
