3634930aed4c7d262838e3953f7c7c9374728115
mmarcel <Marcel.MORAIS-LUNA@ext.ec.europa.eu>
2025-03-24 16:00:59 +0100

    page changes
    components changed

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -11,7 +11,7 @@ import { ProficiencyComponent } from './proficiency/proficiency.component';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { HttpLoaderFactory } from '../general/httpLoaderFactory';
-import { EducationComponent } from './education/education.component';
+import {MoreProyectsComponent} from './more-proyects/more-proyects.component';
 
 
 
@@ -22,7 +22,7 @@ import { EducationComponent } from './education/education.component';
     AboutComponent,
     ExperienceComponent,
     ProficiencyComponent,
-    EducationComponent,
+    MoreProyectsComponent,
     ContactComponent
   ],
   imports: [
