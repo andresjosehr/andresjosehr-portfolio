c18143467257e274711af1186026cddb0daa2c00
Andres <andresjosehr@gmail.com>
2025-07-01 12:56:32 -0400

    Configuración para despliegue automatico

--- production.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ng build --configuration production
-git checkout production
-find . -maxdepth 1 -type f -not \( -name ".git" -or -name ".gitignore" -or -name "production.sh" \) -delete
-rm -rf assets
-cp -r dist/andresjosehr-portfolio/* ./
-cp dist/andresjosehr-portfolio/.htaccess ./.htaccess
